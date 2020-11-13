78503573eb9a12302832406e23c9432595c0dd27
MeguMan <dimon200019@gmail.com>
2020-11-13 18:07:49 +0300

    добавил метод перевода денег

--- internal/apiserver/server.go
+++ internal/apiserver/server.go
@@ -30,8 +30,9 @@ func NewServer(store postgres_store.Store) *server {
 }
 
 func (s *server) configureRouter() {
-	s.router.HandleFunc("/users/{id}/balance/add", s.AddUserBalance()).Methods("PUT")
+	s.router.HandleFunc("/users/{id}/balance/add", s.IncreaseUserBalance()).Methods("PUT")
 	s.router.HandleFunc("/users/{id}/balance/reduce", s.ReduceUserBalance()).Methods("PUT")
+	s.router.HandleFunc("/users/{sender_id}/balance/transfer/{receiver_id}", s.TransferUserBalance()).Methods("PUT")
 	s.router.HandleFunc("/users/{id}/balance", s.GetUserBalance()).Methods("GET")
 }
 
@@ -46,7 +47,7 @@ func (s *server) GetUserBalance() func(w http.ResponseWriter, r *http.Request) {
 		ur := s.store.User()
 
 		var err error
-		u.Balance, err = ur.GetBalanceById(u)
+		u.Balance, err = ur.GetBalanceById(u.ID)
 		if err != nil {
 			log.Print(err)
 			return
@@ -57,7 +58,7 @@ func (s *server) GetUserBalance() func(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (s *server) AddUserBalance() func(w http.ResponseWriter, r *http.Request) {
+func (s *server) IncreaseUserBalance() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		u := model.User{}
 		vars := mux.Vars(r)
@@ -70,7 +71,7 @@ func (s *server) AddUserBalance() func(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		ur := s.store.User()
-		err = ur.AddBalance(u)
+		err = ur.IncreaseBalance(u)
 		if err != nil {
 			log.Print(err)
 			return
@@ -78,6 +79,32 @@ func (s *server) AddUserBalance() func(w http.ResponseWriter, r *http.Request) {
 
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "Balance was increased")
+	}
+}
+
+func (s *server) TransferUserBalance() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		balanceForTransfer := r.URL.Query().Get("balance")
+		receiver := model.User{}
+		sender := model.User{}
+
+		vars := mux.Vars(r)
+		sender.ID, _ = strconv.Atoi(vars["sender_id"])
+		sender.Balance, _ = strconv.Atoi(balanceForTransfer)
+		receiver.ID, _ = strconv.Atoi(vars["receiver_id"])
+		receiver.Balance, _ = strconv.Atoi(balanceForTransfer)
+
+
+		w.Header().Set("Content-Type", "application/json")
+		ur := s.store.User()
+		err := ur.TransferBalance(sender, receiver)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "Balance was transfered")
 	}
 }
 
@@ -101,7 +128,7 @@ func (s *server) ReduceUserBalance() func(w http.ResponseWriter, r *http.Request
 		}
 
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "Balance was decreased")
+		fmt.Fprint(w, "Balance was reduced")
 	}
 }
 
--- internal/store/postgres_store/user_repo.go
+++ internal/store/postgres_store/user_repo.go
@@ -10,8 +10,11 @@ type UserRepository struct {
 	store *Store
 }
 
-func (r *UserRepository) AddBalance(u model.User) error {
-	currentBalance, err := r.GetBalanceById(u)
+func (r *UserRepository) IncreaseBalance(u model.User) error {
+	currentBalance, err := r.GetBalanceById(u.ID)
+	if err != nil {
+		return err
+	}
 
 	_, err = r.store.conn.Exec(context.Background(),"UPDATE users SET balance=$1 where user_id=$2",
 		currentBalance + u.Balance, u.ID)
@@ -20,7 +23,10 @@ func (r *UserRepository) AddBalance(u model.User) error {
 }
 
 func (r *UserRepository) ReduceBalance(u model.User) error {
-	currentBalance, err := r.GetBalanceById(u)
+	currentBalance, err := r.GetBalanceById(u.ID)
+	if err != nil {
+		return err
+	}
 	if u.Balance > currentBalance {
 		return errors.New("balance can't be less than 0")
 	}
@@ -31,10 +37,19 @@ func (r *UserRepository) ReduceBalance(u model.User) error {
 	return err
 }
 
-func (r *UserRepository) GetBalanceById(u model.User) (int, error) {
+func (r *UserRepository) GetBalanceById(id int) (int, error) {
 	var balance int
 	err := r.store.conn.QueryRow(context.Background(),"SELECT balance FROM users WHERE user_id=$1",
-		u.ID).Scan(&balance)
+		id).Scan(&balance)
 
 	return balance, err
+}
+
+func (r *UserRepository) TransferBalance(sender, receiver model.User) error {
+	err := r.ReduceBalance(sender)
+	if err != nil {
+		 return err
+	}
+	err = r.IncreaseBalance(receiver)
+	return err
 }
